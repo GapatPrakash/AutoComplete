c8e228f41ac9b2070fed900eb9d0e42a3f976273
gapatprakash@gmail.com <gapatprakash@gmail.com>
2017-02-28 15:06:24 +0530

    Added the code to github

---
+++ Student.ashx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+using System.Configuration;
+
+namespace JqueryDemo
+{
+    /// <summary>
+    /// Summary description for Student
+    /// </summary>
+    public class Student : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+           string term = context.Request["term"] ?? "";
+
+            string cs = ConfigurationManager.ConnectionStrings["studentcs"].ConnectionString;
+
+            List<string> studentsnames = new List<string>();
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("spGetStudentNames", con);
+
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+                cmd.Parameters.AddWithValue("term", term.ToString());
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+
+                    studentsnames.Add(rdr["Name"].ToString());
+
+                }
+
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            context.Response.Write(js.Serialize(studentsnames));
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="JqueryDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="https://code.jquery.com/jquery-1.11.2.js"></script>
+    <script src="jquery-ui.js"></script>
+    <link href="jquery-ui.css" rel="stylesheet" />
+     
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#txtName').autocomplete({
+
+                source:'student.ashx'
+            });
+
+        });
+    </script>
+  
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+   Student Name :
+        <asp:TextBox ID="txtName" runat="server">
+        </asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
